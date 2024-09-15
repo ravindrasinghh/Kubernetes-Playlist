0f1b1ecd414390ed9800107e00e1d153ee826195
Ravindra Singh <ravindrasinghh.me@gmail.com>
2024-09-15 21:33:27 +0530

    Create argocd.tf

---
+++ Lesson4/argocd.tf
@@ -0,0 +1,10 @@
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "3.35.4"
+  values           = [file("./argocd.yaml")]
+}
+# helm install argocd -n argocd -f values/argocd.yaml
