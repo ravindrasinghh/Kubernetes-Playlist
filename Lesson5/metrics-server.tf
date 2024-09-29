0aab72bc2fb2fc928faef476948521dead4e8131
Ravindra Singh <ravindrasinghh.me@gmail.com>
2024-09-29 10:15:21 +0530

    Create metrics-server.tf

---
+++ Lesson5/metrics-server.tf
@@ -0,0 +1,7 @@
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  version    = "3.12.0"
+  namespace  = "kube-system"
+}
